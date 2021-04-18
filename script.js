a63c48aa73c1e30a17a550ec7c36e18b5914fed5
arogya <arogyabichpuria16@gmail.com>
2021-04-18 10:59:01 +0530

    buided a bad looking prototype of todo list

--- script.js
+++ script.js
@@ -1,11 +1,65 @@
+
+let taskCount=0;
+let taskCompleted=0;
+
+
+
 document.getElementById("addToList").addEventListener("click",()=>{
- let taskdescription=document.getElementById("taskDescription");
- let task=document.getElementById("taskTodo").value;
- 
- 
- let addTask=document.createElement("p");
- addTask.setAttribute('class', "taskAdded");
- addTask.textContent=task;
- taskdescription.appendChild(addTask);
- task="";
+ createTask();
+ incrementTask();
+
 });
+
+
+function replaceIcon(event){
+    let icon=event.target;
+    console.log(icon);
+    icon.classList.remove("far");
+    icon.classList.remove("fa-circle");
+    icon.classList.add("fas");
+    icon.classList.add("fa-check-circle");
+}
+
+function taskCompletedIncrement(){
+    let completed=document.getElementById("completed");
+    taskCompleted+=1;
+    completed.textContent="task Completed:" + taskCompleted;
+}
+
+function incrementTask(){
+    let count=document.getElementById("added");
+    taskCount+=1;
+    count.textContent="task Added:" + taskCount;
+}
+
+function taskContainer(){
+    let taskdescription=document.getElementById("taskDescription");
+    let task=document.getElementById("taskTodo").value;
+    document.getElementById("taskTodo").value="";
+    
+    let container=document.createElement("div");
+    let addTask=document.createElement("p");
+    addTask.classList.add("taskDetails");
+    let icon=document.createElement("i");
+    icon.classList.add("far");
+    icon.classList.add("fa-circle");
+    icon.classList.add("icon");
+    container.setAttribute('class', "taskAdded");
+    addTask.textContent=task;
+    container.appendChild(icon);
+    container.appendChild(addTask);
+    taskdescription.appendChild(container);
+}
+
+function createTask(){
+    
+    taskContainer();
+    document.querySelectorAll(".icon").forEach(item=>{
+        item.addEventListener("click",event=>{
+           replaceIcon(event);
+           taskCompletedIncrement();
+           shiftToCompleted(task);
+        });
+    })
+}
+
