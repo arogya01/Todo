68b124417642e4e2fed16c8b9915f505208e81d4
arogya <arogyabichpuria16@gmail.com>
2021-05-04 16:48:10 +0530

    many changes are required to localstg

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 
-let taskCount=0;
-let taskCompleted=0;
+let taskCount = 0;//keep track of number of tasks
+let taskCompleted = 0;//keep track of number of tasks completed
+let count=0;
 
+if(localStorage.getItem("taskdetails0")){
+   let c=localStorage.getItem("count");
 
+   for(let i=0;i<=1;i++){
+    let text=localStorage.getItem(`taskdetails${i}`);
+    taskContainer(text);
+   }
+    
+ }
 
-document.getElementById("btn").addEventListener("click",()=>{
- createTask();
- incrementTask();
-
+document.getElementById("btn").addEventListener("click", () => {
+    createTask();//create the task 
+    incrementTask();//increment task count
 });
 
-
-function replaceIcon(event){
-    let icon=event.target;
+//replace the empty circle with full circle
+function replaceIcon(event) {
+    let icon = event.target;
     console.log(icon);
     icon.classList.remove("far");
     icon.classList.remove("fa-circle");
@@ -20,52 +28,94 @@ function replaceIcon(event){
     icon.classList.add("fa-check-circle");
 }
 
-function taskCompletedIncrement(){
-    let completed=document.getElementById("completed");
-    taskCompleted+=1;
-    completed.textContent="Task Completed:" + taskCompleted;
+//increment the taskCompleted count
+function taskCompletedIncrement() {
+    let completed = document.getElementById("completed");
+    taskCompleted += 1;
+    completed.textContent = "Task Completed:" + taskCompleted;
 }
 
-function incrementTask(){
-    let count=document.getElementById("added");
-    taskCount+=1;
-    count.textContent="Task Added:" + taskCount;
+//increment task count
+function incrementTask() {
+    let count = document.getElementById("added");
+    taskCount += 1;
+    count.textContent = "Task Added:" + taskCount;
 }
 
-function taskContainer(){
-    let taskdescription=document.getElementById("taskDescription");
-    let task=document.getElementById("taskTodo").value;
-    document.getElementById("taskTodo").value="";
+function taskContainer(text) {
+    let taskdescription = document.getElementById("taskDescription");
     
-    let container=document.createElement("div");
-    let addTask=document.createElement("p");
+
+    let container = document.createElement("div");
+    let addTask = document.createElement("p");
+
     addTask.classList.add("taskDetails");
-    let icon=document.createElement("i");
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("deleteicon");
+    deleteIcon.classList.add("fas");
+    deleteIcon.classList.add("fa-trash");
+   
+
+    let icon = document.createElement("i");
     icon.classList.add("far");
     icon.classList.add("fa-circle");
     icon.classList.add("icon");
+
+
     container.setAttribute('class', "taskAdded");
-    addTask.textContent=task;
+    addTask.textContent = text;
     container.appendChild(icon);
     container.appendChild(addTask);
+    container.appendChild(deleteIcon);
     taskdescription.appendChild(container);
+    setItems(text);
 }
 
-function cutlineThrough(event){
- let current=event.target;
- let para=current.nextSibling;
- para.style.textDecoration="line-through";
+function cutlineThrough(event) {
+    let current = event.target;
+    let para = current.nextSibling;
+    para.style.textDecoration = "line-through";
 }
 
-function createTask(){
+
+function setItems(text){
+  // localStorage.setItem(`taskdetails${taskCount}`,document.querySelector(".taskDetails").textContent);
+  count++;
+  localStorage.setItem(`taskdetails${count}`,text);
+  // localStorage.setItem(`icon${count}`,document.querySelector(".icon").classList.value);
+  // localStorage.setItem(`deleteicon${count}`,document.querySelector(".deleteicon").classList.value);
+   localStorage.setItem("count",count);
+}
+
+function createTask() {
+    let task = document.getElementById("taskTodo").value;//
+    document.getElementById("taskTodo").value = "";//clear up the input field
+    taskContainer(task);
     
-    taskContainer();
-    document.querySelectorAll(".icon").forEach(item=>{
-        item.addEventListener("click",event=>{
-           replaceIcon(event);
-           taskCompletedIncrement();
-           cutlineThrough(event);
+    
+
+    document.querySelectorAll(".icon").forEach(item => {
+        item.addEventListener("click", event => {
+            replaceIcon(event);//to replace with full circle when clicked on it. 
+            taskCompletedIncrement();//increment the taskcompleted count
+            cutlineThrough(event);//put a line over task
         });
     })
+   
+    function removeFromLocalStg(){
+     
+    }
+
+    //event listner to remove the task whence clicked. 
+    document.querySelectorAll(".deleteicon").forEach(item => {
+        item.addEventListener("click", event => {
+            let removeTask = event.target;
+            let parent = removeTask.parentElement;//extracting the parent from the deleteicon(child element)
+            parent.remove();
+            removeFromLocalStg();
+        })
+    })
 }
+
+
 
