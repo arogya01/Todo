dc0574a0d6007d4c4f42d5c4aa823c6f47bf6d97
arogya <arogyabichpuria16@gmail.com>
2021-05-09 00:08:38 +0530

    final

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+let taskCount = 0; //keep track of number of tasks
+let taskCompleted = 0; //keep track of number of tasks completed
 
-let taskCount = 0;//keep track of number of tasks
-let taskCompleted = 0;//keep track of number of tasks completed
-let count=0;
 
-if(localStorage.getItem("taskdetails0")){
-   let c=localStorage.getItem("count");
+if (localStorage.getItem("taskCount")) {
 
-   for(let i=0;i<=1;i++){
-    let text=localStorage.getItem(`taskdetails${i}`);
-    taskContainer(text);
-   }
-    
- }
+    let tadd = localStorage.getItem("taskCount");
+    let count = document.getElementById("added");
+    taskCount = tadd;
+    count.textContent = "Task Added:" + tadd;
+
+
+    let tcom = localStorage.getItem("taskCompleted");
+    let com = document.getElementById("completed");
+    taskCompleted = tcom;
+    com.textContent = "Task Completed:" + taskCompleted;
+
+
+    for (let i = 1; i <= tadd; i++) {
+        let text = localStorage.getItem(`t${i}`);
+        let icont = localStorage.getItem(`icon${i}`);
+        taskContainer(text, icont);
+
+    }
+    eventListners();
+}
+/*
+else {
+    localStorage.setItem("taskCount", 0);
+    localStorage.setItem("taskCompleted", 0);
+}
+*/
 
 document.getElementById("btn").addEventListener("click", () => {
-    createTask();//create the task 
-    incrementTask();//increment task count
+    createTask(); //create the task 
+
 });
 
 //replace the empty circle with full circle
@@ -33,6 +51,8 @@ function taskCompletedIncrement() {
     let completed = document.getElementById("completed");
     taskCompleted += 1;
     completed.textContent = "Task Completed:" + taskCompleted;
+
+    localStorage.getItem("taskCompleted", taskCompleted);
 }
 
 //increment task count
@@ -42,24 +62,33 @@ function incrementTask() {
     count.textContent = "Task Added:" + taskCount;
 }
 
-function taskContainer(text) {
+function taskContainer(text, iconType = "uncheck") {
     let taskdescription = document.getElementById("taskDescription");
-    
+
 
     let container = document.createElement("div");
     let addTask = document.createElement("p");
 
     addTask.classList.add("taskDetails");
+    addTask.setAttribute("id", `t${taskCount}`);
+
     let deleteIcon = document.createElement("i");
     deleteIcon.classList.add("deleteicon");
     deleteIcon.classList.add("fas");
     deleteIcon.classList.add("fa-trash");
-   
+    deleteIcon.setAttribute("id", `delicon${taskCount}`);
+
 
     let icon = document.createElement("i");
-    icon.classList.add("far");
-    icon.classList.add("fa-circle");
-    icon.classList.add("icon");
+    if (iconType === "check") {
+        icon.classList.add("fas");
+        icon.classList.add("fa-check-circle");
+    } else if (iconType === "uncheck") {
+        icon.classList.add("far");
+        icon.classList.add("fa-circle");
+        icon.classList.add("icon");
+    }
+    icon.setAttribute("id", `icon${taskCount}`);
 
 
     container.setAttribute('class', "taskAdded");
@@ -69,6 +98,7 @@ function taskContainer(text) {
     container.appendChild(deleteIcon);
     taskdescription.appendChild(container);
     setItems(text);
+
 }
 
 function cutlineThrough(event) {
@@ -78,44 +108,61 @@ function cutlineThrough(event) {
 }
 
 
-function setItems(text){
-  // localStorage.setItem(`taskdetails${taskCount}`,document.querySelector(".taskDetails").textContent);
-  count++;
-  localStorage.setItem(`taskdetails${count}`,text);
-  // localStorage.setItem(`icon${count}`,document.querySelector(".icon").classList.value);
-  // localStorage.setItem(`deleteicon${count}`,document.querySelector(".deleteicon").classList.value);
-   localStorage.setItem("count",count);
+function setItems(text) {
+    localStorage.setItem(`t${taskCount}`, text);
+    localStorage.setItem(`icon${taskCount}`, "uncheck");
+    localStorage.setItem("taskCount", taskCount);
+    localStorage.setItem("taskCompleted", taskCompleted);
 }
 
+
+
+
+
 function createTask() {
-    let task = document.getElementById("taskTodo").value;//
-    document.getElementById("taskTodo").value = "";//clear up the input field
-    taskContainer(task);
-    
-    
+    let task = document.getElementById("taskTodo").value; //
+    document.getElementById("taskTodo").value = ""; //clear up the input field
+    incrementTask();
+    taskContainer(task, "uncheck");
+    eventListners();
+}
+
+
+function removeFromLocalStg(e) {
+    console.log(e.target.id);
+    let str = e.target.id;
+    let index = str.charAt(str.length - 1);
+    localStorage.removeItem(`t${index}`);
+    localStorage.removeItem(`icon${index}`);
+    localStorage.removeItem(`delicon${index}`);
+}
+
+function editLocalStg(e) {
+    console.log(e.target.id);
+    localStorage.setItem(e.target.id, "check");
+}
+
+function eventListners() {
 
     document.querySelectorAll(".icon").forEach(item => {
         item.addEventListener("click", event => {
-            replaceIcon(event);//to replace with full circle when clicked on it. 
-            taskCompletedIncrement();//increment the taskcompleted count
-            cutlineThrough(event);//put a line over task
+            replaceIcon(event); //to replace with full circle when clicked on it. 
+            taskCompletedIncrement(); //increment the taskcompleted count
+            cutlineThrough(event); //put a line over task
+            editLocalStg(event);
         });
     })
-   
-    function removeFromLocalStg(){
-     
-    }
+
+
 
     //event listner to remove the task whence clicked. 
     document.querySelectorAll(".deleteicon").forEach(item => {
         item.addEventListener("click", event => {
             let removeTask = event.target;
-            let parent = removeTask.parentElement;//extracting the parent from the deleteicon(child element)
+            let parent = removeTask.parentElement; //extracting the parent from the deleteicon(child element)
             parent.remove();
-            removeFromLocalStg();
+            removeFromLocalStg(e);
         })
     })
+
 }
-
-
-
